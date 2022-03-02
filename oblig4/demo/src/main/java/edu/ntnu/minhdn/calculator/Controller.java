4277f37fee11214bd0b5ac7cbfbb8a280d053402
Minh Dan Nguyen <minhdn@stud.ntnu.no>
2022-03-02 20:10:29 +0100

    initial commit, oblig4

---
+++ oblig4/demo/src/main/java/edu/ntnu/minhdn/calculator/Controller.java
@@ -0,0 +1,7 @@
+package edu.ntnu.minhdn.calculator;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+}
---
+++ oblig4/demo/src/test/java/edu/ntnu/minhdn/calculator/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.minhdn.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
